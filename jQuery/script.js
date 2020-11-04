06f115cb0d1f5807c21ea9dfa0af1760e2b141af
EranHolzman <eranhol1993@gmail.com>
2020-11-04 23:33:54 +0200

    Finish exercises

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,6 +1,15 @@
-$("#div1").hover(function () {
-    $("#div1").css("background-color", "blue")
+//Exercise 1
+
+$('#myButton').click(function () {
+    $('#myUl').append('<li>' + $('#myInput').val() + '</li>');
 })
+
+
+// Exercise 2
+
+$('body').on('click', 'li', function() {
+    $(this).remove();
+});
 
 
 
